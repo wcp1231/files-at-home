7507853f61acc209155d1418b88264ee986482a7
wcp1231 <2058684+wcp1231@users.noreply.github.com>
2025-03-15 15:32:03 +0800

    启用 Geist 和 Geist_Mono 字体，并更新布局样式

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/Header";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "FolderPort",
@@ -27,8 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body 
-        // className={`min-h-screen bg-background font-sans ${geistSans.variable} ${geistMono.variable} antialiased`}
-        className={`min-h-screen bg-background font-sans antialiased`}
+        className={`min-h-screen bg-background font-sans ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
